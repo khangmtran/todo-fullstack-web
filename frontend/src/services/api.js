4c57190f84ec2cd23d5bbf3523021109a750b6d6
Khang Tran <khangminh1472001@gmail.com>
2025-08-11 12:03:27 -0700

    deploying FE...

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,32 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
 });
 
-//Interceptor: before any request, do something
-
-//add token to any request
-//ret for return
 api.interceptors.request.use((retApi) => {
   const token = localStorage.getItem("token");
-  if (token) {
-    retApi.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) retApi.headers.Authorization = `Bearer ${token}`;
   return retApi;
 });
 
-//handle expired token
 api.interceptors.response.use(
-  //if token is not expired
-  (response) => response,
-  //if token is expired
-  (error) => {
-    if (error.response?.status === 401) {
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
       localStorage.removeItem("token");
       window.location.href = "/login";
     }
-    return Promise.reject(error);
+    return Promise.reject(err);
   }
 );
 
