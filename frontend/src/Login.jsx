39ca93791d22ec8bb70df54c3f16840349232a8e
Khang Tran <khangminh1472001@gmail.com>
2025-08-11 18:52:28 -0700

    fix FE bug

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -35,13 +35,7 @@ function Login({ onLogin }) {
 
   return (
     <div className="login-wrapper">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit(e);
-        }}
-        className="login-form"
-      >
+      <div className="login-form">
         <h2>Login</h2>
 
         {wrongInfo && (
@@ -65,14 +59,14 @@ function Login({ onLogin }) {
           className="form-input"
         />
 
-        <button type="submit" className="btn-login">
+        <button type="button" onClick={handleSubmit} className="btn-login">
           Log In
         </button>
         <p>Or Sign Up Using</p>
         <button type="button" className="btn-signup" onClick={handleSignup}>
           Sign Up
         </button>
-      </form>
+      </div>
     </div>
   );
 }
